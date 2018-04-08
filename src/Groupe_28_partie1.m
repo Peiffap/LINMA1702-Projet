ab103ea79bd76ae196dca2ad7fbb7415e653a578
NizarBousselmi <36970357+NizarBousselmi@users.noreply.github.com>
2018-04-08 10:35:45 +0200

    Ajout d'un input pour l'affichage

--- src/Groupe_28_partie1.m
+++ src/Groupe_28_partie1.m
@@ -1,4 +1,4 @@
-function [plan_panneaux, plan_heure_sup, plan_sous_traitant, plan_stockage, autres_vars] = Groupe_28_partie1
+function [plan_panneaux, plan_heure_sup, plan_sous_traitant, plan_stockage, autres_vars] = Groupe_28_partie1(recalcul)
 % Groupe_28_partie1  organise le planning d'une usine de production de
 % panneaux solaires. Cette fonction a ete realisee dans le cadre du projet
 % du cours de Modèles et méthodes d'optimisation I donne par Francois
@@ -7,11 +7,25 @@ function [plan_panneaux, plan_heure_sup, plan_sous_traitant, plan_stockage, autr
 % Plus precisement, la fonction lit les donnees contenues dans le fichier
 % data_partie1.m et renvoie les actions hebdomadaires que doit faire
 % l'usine si elle veut minimiser son cout. Elle affiche dans un tableau les
-% resultats et dans un graphique l'evolution du cout total si la demande
-% est augmentee d'un vecteur epsilon*delta_demande ou epsilon est un
-% scalaire entre 0 et 1. Ce sont les valeurs d'epsilon qui sont sur
-% l'abscisse du graphique.
-% 
+% resultats de l'optimisation et dans un graphique une estimation de
+% l'evolution du cout total si la demande est augmentee d'un vecteur
+% epsilon*delta_demande ou epsilon est un scalaire entre 0 et 1. Ce sont
+% les valeurs d'epsilon qui sont sur l'abscisse du graphique. Si le booleen
+% "recalcul" est vrai (=1) alors le probleme est entierement recalcule avec
+% les nouvelles valeurs de la demande et le nouveau cout exacte est affiche
+% sur le graphique. Si recalcul=0 alors seulement l'estimation est
+% calculee et affichee.
+% Attention: si T est tres grand, le programme peut prendre un certain
+% temps si on recalcule entierement le probleme pour chaque nouvelle
+% demande.
+
+% Entree:
+%  recalcul : booleen. Si il est vrai (=1), en plus de l'estimation, le
+%             probleme est entierement recalcule, et le nouveau cout total
+%             affiche, pour chaque nouvelle valeur de
+%             demande + epsilon*delta_demande. Si il est faux (=0),
+%             seulement l'estimation est calculee et affichee.
+%
 % Sorties:
 %  plan_panneaux : vecteur T x 1. Contient le nombre de panneaux produits
 %                  par l'usine chaque semaine.
@@ -28,8 +42,8 @@ function [plan_panneaux, plan_heure_sup, plan_sous_traitant, plan_stockage, autr
 %               premier contient le nombre de panneaux fournis pour la
 %               semaine i. Le deuxieme contient le nombre de panneaux
 %               fournis pour la semaine i-1. Le troisieme contient le
-%               nombre de panneaux fournis pour la semaine i-2. 
-% 
+%               nombre de panneaux fournis pour la semaine i-2.
+%
 % Auteurs   : Andres Zarza Davila, Nizar Bousselmi et Gilles Peiffer
 % Groupe    : 28
 
@@ -61,6 +75,9 @@ else
 end
 
 %Verifie les donnees entrees
+if recalcul ~=0 && recalcul~=1
+    disp('Attention: recalcul doit valoir 1 si la valeur exacte de chaque nouvelle demande doit etre calculee,')
+    disp('et 0 sinon et pas un autre nombre.')
 if heures_par_jour>24 || heures_par_jour<0
     disp('ATTENTION: Nombre innatendu d''heures par jour :-(')
 end
@@ -108,22 +125,22 @@ b=[ ones(T,1)*heures_par_semaine*nb_ouvriers;
 
 if T==1
     Aeq=[ sparse(1,4*T) 1 sparse(1,2);
-          1 sparse(1,T-1) -1 sparse(1,2*T-1) 1 sparse(1,T-1) -1 sparse(1,T-1) -1 zeros(1,T-1) -1 zeros(1,T-1)];
-   
-    beq=[ demande';
-          sparse(1,1)];
-else
-    Aeq=[ sparse(T,4*T) E sparse(T,1) E(:,1:end-1) sparse(T,2) E(:,1:end-2);
-          E ([E(:,2:end) sparse(T,1)]-E) sparse(T,T) E -E -E -E
-          sparse(1,5*T) 1 sparse(1,2*T-1);  %x6,1=0
-          sparse(1,6*T) 1 sparse(1,T-1);    %x7,1=0
-          sparse(1,6*T+1) 1 sparse(1,T-2)]; %x7,2=0
+        1 sparse(1,T-1) -1 sparse(1,2*T-1) 1 sparse(1,T-1) -1 sparse(1,T-1) -1 zeros(1,T-1) -1 zeros(1,T-1)];
     
     beq=[ demande';
-          -stock_initial;
-          sparse(T-2,1);
-          stock_initial;
-          sparse(3,1)]; %x6,1=x7,1=x7,2=0
+        sparse(1,1)];
+else
+    Aeq=[ sparse(T,4*T) E sparse(T,1) E(:,1:end-1) sparse(T,2) E(:,1:end-2);
+        E ([E(:,2:end) sparse(T,1)]-E) sparse(T,T) E -E -E -E
+        sparse(1,5*T) 1 sparse(1,2*T-1);  %x6,1=0
+        sparse(1,6*T) 1 sparse(1,T-1);    %x7,1=0
+        sparse(1,6*T+1) 1 sparse(1,T-2)]; %x7,2=0
+    
+    beq=[ demande';
+        -stock_initial;
+        sparse(T-2,1);
+        stock_initial;
+        sparse(3,1)]; %x6,1=x7,1=x7,2=0
 end
 
 lb=sparse(7*T,1);
@@ -150,7 +167,7 @@ objBase=objBase+salaire;
 
 %Stock initial de chaque semaine
 stock_initial_semaine=[stock_initial plan_stockage(1:end-1)'];
- 
+
 %Production mise en stock
 mis_en_stock=plan_stockage'-stock_initial_semaine;
 mis_en_stock(end)=stock_initial-stock_initial_semaine(end);
@@ -177,8 +194,8 @@ fprintf('Production via heures sup :%s\n',sprintf('%11.1f',x(:,3)/duree_assembla
 fprintf('Production sous-traitee   :%s\n',sprintf('%11.1f',x(:,4)))
 fprintf('-> Production totale      :%s\n',sprintf('%11.1f',x(:,1)+x(:,4)))
 fprintf('\n')
-fprintf('Demande a satisfaire      :%s\n',sprintf('%11.1f',demande)) 
-fprintf('Demande satis a temps     :%s\n',sprintf('%11.1f',x(:,5))) 
+fprintf('Demande a satisfaire      :%s\n',sprintf('%11.1f',demande))
+fprintf('Demande satis a temps     :%s\n',sprintf('%11.1f',x(:,5)))
 fprintf('Demande non satisfaite    :%s\n',sprintf('%11.1f',demande'-x(:,5)))
 fprintf('Demande satis apres 1 sem :%s\n',sprintf('%11.1f',fournis_1semaine))
 fprintf('Demande satis apres 2 sem :%s\n',sprintf('%11.1f',fournis_2semaines))
@@ -186,44 +203,59 @@ fprintf('Production mise en stock  :%s\n',sprintf('%11.1f',mis_en_stock))
 fprintf('\n')
 
 %Parcourt des differentes valeurs de epsilon
-%Ici on recalcule completement tout le probleme
-for i=1:N
-    %Modification du vecteur demande
-    demande2=demande+epsilon(i)*delta_demande;
-    if T==1
-        beq=[ demande2';
-              sparse(1,1)];
-    else
-        beq=[ demande2';
-              -stock_initial;
-              sparse(T-2,1);
-              stock_initial;
-              sparse(3,1)];
+%Ici on recalcule completement tout le probleme si recalcul=1
+if recalcul==1
+    for i=1:N
+        %Modification du vecteur demande
+        demande2=demande+epsilon(i)*delta_demande;
+        if T==1
+            beq=[ demande2';
+                sparse(1,1)];
+        else
+            beq=[ demande2';
+                -stock_initial;
+                sparse(T-2,1);
+                stock_initial;
+                sparse(3,1)];
+        end
+        
+        [~,obj,exitFlag] = linprog(c,A,b,Aeq,beq,lb,[],options);
+        if exitFlag~=1
+            fprintf('Le probleme ne semble pas avoir de solution en epsilon=%d (i=%d)\n',epsilon(i),i)
+            disp('Le programme a ete arrete.')
+            break
+        else
+            y(i)=obj;
+        end
     end
-   
-    [~,obj,exitFlag] = linprog(c,A,b,Aeq,beq,lb,[],options);
-    if exitFlag~=1
-        fprintf('Le probleme ne semble pas avoir de solution en epsilon=%d (i=%d)\n',epsilon(i),i)
-        disp('Le programme a ete arrete.')
-        break      
-    else
-        y(i)=obj;
-    end
+    %On recupere la solution du dual pour estimer la variation
+    solution_dual=dual.eqlin(1:T);
+    diff=-delta_demande*solution_dual;
+    
+    %Affichage du graphique
+    figure
+    plot(epsilon,y-objBase+salaire,'r','LineWidth',2)
+    hold on
+    p2=plot(epsilon,epsilon*diff,'--','LineWidth',2);
+    p2.Color='b';
+    title('Variation du cout total en fonction de la variation de la demande','FontSize',18)
+    xlabel('epsilon','FontSize',16)
+    ylabel('Variation du cout [euros]','FontSize',16)
+    legend({'Valeur exacte','Estimation de la procedure'},'FontSize',15)
+    
+    %Si recalcul=0
+else
+    %On recupere la solution du dual pour estimer la variation
+    solution_dual=dual.eqlin(1:T);
+    diff=-delta_demande*solution_dual;
+    
+    %Affichage du graphique
+    figure
+    p2=plot(epsilon,epsilon*diff,'--','LineWidth',2);
+    p2.Color='b';
+    title('Variation du cout total en fonction de la variation de la demande','FontSize',18)
+    xlabel('epsilon','FontSize',16)
+    ylabel('Variation du cout [euros]','FontSize',16)
+    legend({'Estimation de la procedure'},'FontSize',15)
 end
-
-%On recupere la solution du dual pour estimer la variation
-solution_dual=dual.eqlin(1:T);
-diff=-delta_demande*solution_dual;
-
-%Affichage du graphique
-figure
-plot(epsilon,y-objBase+salaire,'r','LineWidth',2)
-hold on
-p2=plot(epsilon,epsilon*diff,'--','LineWidth',2);
-p2.Color='b';
-title('Variation du cout total en fonction de la variation de la demande','FontSize',18)
-xlabel('epsilon','FontSize',16)
-ylabel('Variation du cout [euros]','FontSize',16)
-legend({'Valeur exacte','Estimation de la procedure'},'FontSize',15)
-
 end
